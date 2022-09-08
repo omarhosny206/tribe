2a0d56a36c21fac5d75b437e657a61b94bdfbc84
omarhosny206 <omarhosny102@gmail.com>
2022-09-08 16:22:02 +0200

    Added all dtos [UserDto, PostDto, CommentDto]

---
+++ src/main/java/com/example/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+
+import com.example.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
