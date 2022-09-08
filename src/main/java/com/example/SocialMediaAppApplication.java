d793bdc48fd24a9f96bd6634f4a41bb1da8fbb9a
kareemgalall <60933161+kareemgalall@users.noreply.github.com>
2022-09-08 18:43:34 +0200

    updated project structure

--- src/main/java/com/example/SocialMediaAppApplication.java
+++ src/main/java/com/example/SocialMediaAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SocialMediaAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SocialMediaAppApplication.class, args);
-	}
-
+    public static void main(String... args) {
+        SpringApplication.run(SocialMediaAppApplication.class, args);
+    }
 }
