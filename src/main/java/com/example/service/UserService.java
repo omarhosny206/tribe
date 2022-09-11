0d2bbf29372f10b0edd87952ddb69b60440048c0
kareemgalall <60933161+kareemgalall@users.noreply.github.com>
2022-09-11 20:09:40 +0200

    create UserService

---
+++ src/main/java/com/example/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.service;
+
+import com.example.entity.User;
+import com.example.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user=userRepository.findByEmail(username);
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),new ArrayList<>());
+    }
+}
