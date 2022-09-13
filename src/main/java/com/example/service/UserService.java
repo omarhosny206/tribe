abd889b0411754c98e289c302a39aff9518ffbf0
omarhosny206 <omarhosny102@gmail.com>
2022-09-13 13:45:18 +0200

    Added UserService

--- src/main/java/com/example/service/UserService.java
+++ src/main/java/com/example/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.service;
 import com.example.entity.User;
 import com.example.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,10 +15,29 @@ import java.util.ArrayList;
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
+
+    public User getById(long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User getByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public ResponseEntity<String> follow(String username) {
+        User user = getByUsername(username);
+
+        if(user == null) {
+            return new ResponseEntity<>("User not found", HttpStatus.BAD_REQUEST);
+        }
+
+        user.getFollowers().add(user);
+        return new ResponseEntity<>("Followed successfully", HttpStatus.OK);
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user=userRepository.findByEmail(username);
         return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),new ArrayList<>());
     }
-
 }
