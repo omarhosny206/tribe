1ab0c260b169c36ad1f5d063beb68540be88d6e8
Omar Hosny Keshk <omarhosny102@gmail.com>
2024-01-05 16:38:59 +0200

    Update TribeRepository.java

--- src/main/java/com/tribe/repository/TribeRepository.java
+++ src/main/java/com/tribe/repository/TribeRepository.java
@@ -1,13 +1,19 @@
 package com.tribe.repository;
 
 import com.tribe.entity.Tribe;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TribeRepository extends JpaRepository<Tribe, Long> {
+    @EntityGraph(value = "Tribe.user")
+    @Override
+    Optional<Tribe> findById(Long aLong);
+
     Tribe findByName(String name);
 
     List<Tribe> findAllByUserId(long userId);
