dd5cde5c8f84c7e493122fcf3a13c090a617d161
Omar Hosny Keshk <omarhosny102@gmail.com>
2024-02-01 22:33:18 +0200

    Add tests for TribeApplication

---
+++ src/test/java/com/tribe/TribeApplicationTest.java
@@ -0,0 +1,11 @@
+package com.tribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TribeApplicationTest {
+
+}
