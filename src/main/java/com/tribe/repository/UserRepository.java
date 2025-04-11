2539ac0e6c22d4a59315672eff997944377d5705
Omar Hosny Keshk <omarhosny102@gmail.com>
2025-04-11 17:21:55 +0200

    Add existsByUsername method

--- src/main/java/com/tribe/repository/UserRepository.java
+++ src/main/java/com/tribe/repository/UserRepository.java
@@ -9,4 +9,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     public User findByUsername(String username);
 
     public User findByEmail(String email);
+
+    public boolean existsByUsername(String charlie);
 }
