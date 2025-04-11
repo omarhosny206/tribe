7fbcf89f58e56d3c770f710c358e0e758553f7f0
Omar Hosny Keshk <omarhosny102@gmail.com>
2025-04-11 17:22:11 +0200

    Update LoginServiceTest.java

--- src/test/java/com/tribe/service/impl/LoginServiceTest.java
+++ src/test/java/com/tribe/service/impl/LoginServiceTest.java
@@ -9,102 +9,73 @@ import com.tribe.util.CustomUser;
 import com.tribe.util.JwtUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-
-import java.util.Collection;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.openMocks;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class LoginServiceTest {
-    @InjectMocks
-    private LoginServiceImpl loginService;
-
     @Mock
     private AuthenticationManager authenticationManager;
 
     @Mock
     private JwtUtil jwtUtil;
 
-    @Mock
-    private AuthenticationUser authenticationUser;
+    @InjectMocks
+    private LoginServiceImpl loginService;
 
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String username;
-    private String password;
-    private String hashedPassword;
     private String accessToken;
     private String refreshToken;
     private User savedUser;
+    private CustomUser customUser;
+
+    private LoginRequestDto loginRequestDto;
+
 
 
     @BeforeEach
     public void setUp() {
-        openMocks(this);
-
-        id = 1L;
-        firstName = "omar";
-        lastName = "hosny";
-        email = "omarhosny102@gmail.com";
-        username = "omarhosny102";
-        password = "12345678";
-        hashedPassword = "####12345678####";
+        long id = 1L;
+        String firstName = "omar";
+        String lastName = "hosny";
+        String email = "omarhosny102@gmail.com";
+        String username = "omarhosny102";
+        String hashedPassword = "####12345678####";
         accessToken = "access-token";
         refreshToken = "refresh-token";
         savedUser = new User(id, firstName, lastName, username, email, hashedPassword);
-    }
-
-    @Test
-    public void shouldThrowExceptionWhenSendingBadCredentials() {
-        // Arrange
-        when(authenticationManager.authenticate(any(Authentication.class)))
-                .thenThrow(BadCredentialsException.class);
-
-        LoginRequestDto loginRequestDto = new LoginRequestDto(email, password);
-
-        // Act
-        Throwable throwable = catchThrowable(() -> loginService.login(loginRequestDto));
-
-        // Assert
-        assertThat(throwable)
-                .isInstanceOf(AuthenticationException.class);
+        customUser = new CustomUser(savedUser);
+        loginRequestDto = new LoginRequestDto(savedUser.getEmail(), savedUser.getPassword());
     }
 
     @Test
     public void shouldLoginSuccessfully() {
         // Arrange
-        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
-                .thenReturn(getAuthentication());
+        Authentication authentication = mock(Authentication.class);
 
-        try (MockedStatic<AuthenticationUser> mocked = mockStatic(AuthenticationUser.class)) {
-            mocked.when(() -> AuthenticationUser.get(any(Authentication.class)))
-                    .thenReturn(savedUser);
-        }
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(authentication);
+
+        when(authentication.getPrincipal()).thenReturn(customUser);
 
         when(jwtUtil.generateAccessToken(anyString()))
                 .thenReturn(accessToken);
 
         when(jwtUtil.generateRefreshToken(anyString()))
                 .thenReturn(refreshToken);
-
-        LoginRequestDto loginRequestDto = new LoginRequestDto(email, password);
 
         // Act
         LoginResponseDto loginResponseDto = loginService.login(loginRequestDto);
@@ -121,42 +92,34 @@ class LoginServiceTest {
                 .isEqualTo(refreshToken);
     }
 
-    private Authentication getAuthentication() {
-        return new Authentication() {
-            @Override
-            public Collection<? extends GrantedAuthority> getAuthorities() {
-                return List.of();
-            }
+    @Test
+    public void shouldThrowExceptionWhenBadCredentials() {
+        // Arrange
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenThrow(BadCredentialsException.class);
 
-            @Override
-            public Object getCredentials() {
-                return new Object();
-            }
+        // Act
+        Throwable throwable = catchThrowable(() -> loginService.login(loginRequestDto));
 
-            @Override
-            public Object getDetails() {
-                return new Object();
-            }
-
-            @Override
-            public Object getPrincipal() {
-                return new CustomUser(savedUser);
-            }
-
-            @Override
-            public boolean isAuthenticated() {
-                return true;
-            }
-
-            @Override
-            public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
-                isAuthenticated = true;
-            }
-
-            @Override
-            public String getName() {
-                return "";
-            }
-        };
+        // Assert
+        assertThat(throwable)
+                .isInstanceOf(BadCredentialsException.class)
+                .isInstanceOf(AuthenticationException.class);
     }
+
+    @Test
+    public void shouldThrowExceptionWhenAuthenticationIsNull() {
+        // Arrange
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(null);
+
+        // Act
+        Throwable throwable = catchThrowable(() -> loginService.login(loginRequestDto));
+
+        // Assert
+        assertThat(throwable)
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    
 }
