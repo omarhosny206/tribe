193ecb50d840a5d0555fcd2c9bd34fc1000a5269
Omar Hosny Keshk <omarhosny102@gmail.com>
2025-04-11 17:59:57 +0200

    Update SignupServiceTest.java

--- src/test/java/com/tribe/service/impl/SignupServiceTest.java
+++ src/test/java/com/tribe/service/impl/SignupServiceTest.java
@@ -2,19 +2,20 @@ package com.tribe.service.impl;
 
 
 import com.tribe.dto.SignupRequestDto;
+import com.tribe.dto.TribeRequestDto;
 import com.tribe.entity.Tribe;
 import com.tribe.entity.User;
 import com.tribe.exception.CustomException;
-import com.tribe.service.TribeService;
-import com.tribe.service.UserService;
 import com.tribe.util.UsernameGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
@@ -22,59 +23,78 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.openMocks;
 
+@ExtendWith(MockitoExtension.class)
 class SignupServiceTest {
+    @Mock
+    private UserServiceImpl userService;
+
+    @Mock
+    private TribeServiceImpl tribeService;
+
+    @Mock
+    private BCryptPasswordEncoder passwordEncoder;
+
     @InjectMocks
     private SignupServiceImpl signupService;
 
-    @Mock
-    private UserService userService;
-
-    @Mock
-    private TribeService tribeService;
-
-    @Mock
-    private PasswordEncoder passwordEncoder;
-
-    @Mock
-    private UsernameGenerator usernameGenerator;
-
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String username;
-    private String password;
-    private String hashedPassword;
-    private String accessToken;
-    private String refreshToken;
-    private User user;
+    private String generatedUsername;
     private String tribeNamePrefix;
+    private User user;
+    private SignupRequestDto signupRequestDto;
+    private Tribe tribe;
 
     @BeforeEach
     public void setUp() {
-        openMocks(this);
+        generatedUsername = "omarhosny102999";
 
-        id = 1L;
-        firstName = "omar";
-        lastName = "hosny";
-        email = "omarhosny102@gmail.com";
-        username = "omarhosny102";
-        password = "12345678";
-        hashedPassword = "####12345678####";
+        long id = 1L;
+        String firstName = "omar";
+        String lastName = "hosny";
+        String email = "omarhosny102@gmail.com";
+        String username = generatedUsername;
+        String hashedPassword = "####12345678####";
         user = new User(id, firstName, lastName, username, email, hashedPassword);
+        signupRequestDto = new SignupRequestDto(firstName, lastName, email, hashedPassword);
+        tribe = new Tribe("own.tribe." + user.getId(), user);
+    }
+
+    @Test
+    public void shouldSignupSuccessfully() {
+        // Arrange
+        when(userService.getByEmailOrNull(anyString()))
+                .thenReturn(null);
+
+        try (MockedStatic<UsernameGenerator> mocked = mockStatic(UsernameGenerator.class)) {
+            mocked.when(() -> UsernameGenerator.generateFromEmail(anyString())).thenReturn(generatedUsername);
+        }
+
+        when(userService.getByUsernameOrNull(anyString()))
+                .thenReturn(null);
+
+        when(passwordEncoder.encode(anyString()))
+                .thenReturn(user.getPassword());
+
+        when(userService.save(any(User.class)))
+                .thenReturn(user);
+
+        when(tribeService.save(any(User.class), any(TribeRequestDto.class)))
+                .thenReturn(tribe);
+
+        // Act
+        User user = signupService.signup(signupRequestDto);
+
+        // Assert
+        assertThat(user)
+                .usingRecursiveAssertion()
+                .isEqualTo(user);
     }
 
     @Test
     public void shouldThrowExceptionWhenEmailIsAlreadyUsed() {
         // Arrange
-        User user = new User();
-
         when(userService.getByEmailOrNull(anyString()))
                 .thenReturn(user);
-
-        SignupRequestDto signupRequestDto = new SignupRequestDto("omar", "hosny", "omarhosny102@gmail.com", "12345678");
 
         // Act
         Throwable throwable = catchThrowable(() -> signupService.signup(signupRequestDto));
@@ -85,40 +105,5 @@ class SignupServiceTest {
 
         assertThat(((CustomException) throwable).getHttpStatus())
                 .isEqualTo(HttpStatus.BAD_REQUEST);
-    }
-
-    @Test
-    public void shouldSignupSuccessfully() {
-        // Arrange
-        when(userService.getByEmailOrNull(anyString()))
-                .thenReturn(null);
-
-        try (MockedStatic<UsernameGenerator> mocked = mockStatic(UsernameGenerator.class)) {
-            mocked.when(() -> UsernameGenerator.generateFromEmail(anyString())).thenReturn(username);
-        }
-
-        when(userService.getByUsernameOrNull(anyString()))
-                .thenReturn(null);
-
-        when(passwordEncoder.encode(anyString()))
-                .thenReturn(null);
-
-        SignupRequestDto signupRequestDto = new SignupRequestDto(firstName, lastName, email, password);
-        User savedUser = new User(id, firstName, lastName, username, email, hashedPassword);
-        Tribe tribe = new Tribe(tribeNamePrefix + savedUser.getId(), savedUser);
-
-        when(userService.save(any(User.class)))
-                .thenReturn(savedUser);
-
-        when(tribeService.save(any(Tribe.class)))
-                .thenReturn(tribe);
-
-        // Act
-        User user = signupService.signup(signupRequestDto);
-
-        // Assert
-        assertThat(user)
-                .usingRecursiveAssertion()
-                .isEqualTo(savedUser);
     }
 }
